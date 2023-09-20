2ac771c81814cc768ec8acc2ffaa36474a576d64
Yukichatte <145517040+Yukichatte@users.noreply.github.com>
2023-09-20 20:15:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const textInput = document.getElementById('textInput');
+    const confirmButton = document.getElementById('confirmButton');
+    const message = document.getElementById('message');
+
+    confirmButton.addEventListener('click', function () {
+        const inputValue = textInput.value.toLowerCase();
+
+        if (inputValue === 'joão') {
+            message.innerHTML = 'NZSXMZLSEBTW63TOMEQGO2LWMUQHS33VEB2XA===';
+        } else if (inputValue === 'never gonna let you down') {
+            message.innerHTML = '<p>Muito bem! Para continuar, clique <a href="https://imgur.com/a/ltzkiSY">aqui</a>.</p>';
+        } else {
+            message.innerHTML = '';
+        }
+    });
+});
